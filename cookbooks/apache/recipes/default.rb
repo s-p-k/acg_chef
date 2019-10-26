c633f3bc9a45e1176b3b18c9de107ef843b9b907
spk <moysp@posteo.net>
2019-10-26 14:33:09 +0000

    Adding the apache cookbook again

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+#
+
+include_recipe 'apache::webserver'
---
+++ cookbooks/apache/recipes/webserver.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: apache
+# Recipe:: webeserver
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+package 'apache2' do
+  action :install
+end
